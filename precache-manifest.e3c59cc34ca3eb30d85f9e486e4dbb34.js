45a60ab95c0501b722f488e2c183ad8b70477621
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2020-07-26 10:49:52 -0400

    Updates

---
+++ precache-manifest.e3c59cc34ca3eb30d85f9e486e4dbb34.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31b9847ee3b93c8c44d3ea6dc9073b1",
+    "url": "/life-credits/index.html"
+  },
+  {
+    "revision": "d7564dd54400cad03bf1",
+    "url": "/life-credits/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6a7ff70096915d60e078",
+    "url": "/life-credits/static/css/main.87fe290d.chunk.css"
+  },
+  {
+    "revision": "d7564dd54400cad03bf1",
+    "url": "/life-credits/static/js/2.a070d3c7.chunk.js"
+  },
+  {
+    "revision": "800c02977d76d137ca141bc95a19c674",
+    "url": "/life-credits/static/js/2.a070d3c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7ff70096915d60e078",
+    "url": "/life-credits/static/js/main.a70aeff9.chunk.js"
+  },
+  {
+    "revision": "e672147075238f658404",
+    "url": "/life-credits/static/js/runtime-main.3ab9a756.js"
+  },
+  {
+    "revision": "9341552d8cbfd150ac426cbd065c51a0",
+    "url": "/life-credits/static/media/Merriweather-Light.9341552d.ttf"
+  },
+  {
+    "revision": "9e2688278bcb7a199bcfd8c8302eade3",
+    "url": "/life-credits/static/media/Moon-Light.9e268827.otf"
+  }
+]);
