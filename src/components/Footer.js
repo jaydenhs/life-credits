24df8237f179c16a3e4beae0dd7c5f3fbd8aeb39
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2020-10-16 22:21:08 -0400

    added more entries and updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ import "bootstrap/dist/css/bootstrap.css";
 function Footer() {
   return (
     <div class="background pb-5">
-      <p class="text-center">
-        Template by{" "}
+      <p class="text-center pb-2">
+        A project by{" "}
         <a
           href="https://github.com/JaydenHsiao/life-credits"
           class="text-white"
@@ -16,6 +16,19 @@ function Footer() {
         >
           Jayden Hsiao
         </a>
+      </p>
+      <p class="text-center">
+        Special thanks to my mentor{" "}
+        <a
+          href="https://sharonzheng.club/"
+          class="text-white"
+          style={{ textDecoration: "underline" }}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sharon Zheng
+        </a>{" "}
+        for helping me bring this project to life!
       </p>
     </div>
   );
